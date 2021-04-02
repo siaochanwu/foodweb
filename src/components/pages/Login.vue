e43db633cbb39ed4d756f522b8f5f62ac0797498
siaochanwu <jill007tw@gmail.com>
2021-04-02 22:39:18 +0800

    fix:開新檔案.設定

---
+++ src/components/pages/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="Login">
+    <form class="form-signin" @submit.prevent="signin">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">UserName</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus v-model="user.username">
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required v-model="user.password">
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" @click="signin">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      user: {
+        username: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    signin() {
+    const api = "https://vue-course-api.hexschool.io/admin/signin";
+    this.axios
+      .post(api, this.user)
+      .then(res => {
+        console.log(res)
+        this.$router.push('/')
+      })
+    }
+  },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//官方
+import Home from '../components/HelloWorld.vue'
+import Login from "@/components/pages/Login"
+
+
+//自訂
+
+Vue.use(VueRouter)//啟用
+
+//定義路徑
+const routes = [
+  {
+    path: "*", //防止隨便改網址
+    redirect: "login",
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,//對應元件
+    meta: { requiresAuth: true }//路油源信息
+
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  
+];
+
+const router = new VueRouter({
+  routes
+})
+
+export default router //輸出
